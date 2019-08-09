611a024cacbfa72f50e0e205aa7a0fa069234daf
dydqja1013 <dydqja1013@naver.com>
2019-08-10 04:35:36 +0900

    final

--- msproject/settings.py
+++ msproject/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'msproject.urls'
@@ -135,3 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 추가 chanho - 19_8_1_19:45
 MEDIA_URL = '/media/' # 추가 chanho - 19_8_1_19:45
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
